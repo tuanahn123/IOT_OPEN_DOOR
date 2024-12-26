960bc66845271ab0aa9f39f213610794dd3423f3
VietAnh30 <vietanhdang487@gmail.com>
2024-12-26 13:56:38 +0700

    fix

--- db.js
+++ db.js
@@ -1,29 +1,34 @@
-'use strict'
-const mongoose = require('mongoose')
-const connectString = 'mongodb+srv://vietanhdang487:Uay7nznVvSuuP1a0@dooriot.24sj1.mongodb.net/?retryWrites=true&w=majority&appName=doorIoT'
+"use strict";
+const mongoose = require("mongoose");
+require("dotenv").config(); // Thêm dòng này để sử dụng dotenv
 
-
+const connectString = process.env.MONGODB_URI; // Lấy chuỗi kết nối từ biến môi trường
 
 class Database {
-    constructor() {
-        this.connect()
+  constructor() {
+    this.connect();
+  }
+
+  connect() {
+    if (!true) {
+      mongoose.set("debug", true);
+      mongoose.set("debug", { color: true });
     }
-    connect() {
-        if (!true) {
-            mongoose.set('debug', true)
-            mongoose.set('debug', { color: true })
-        }
-        mongoose.connect(connectString).then(_ => {
-            console.log('Mongodb Connected')
-        })
-            .catch(err => console.log('Error:: ' + err))
+    mongoose
+      .connect(connectString)
+      .then((_) => {
+        console.log("Mongodb Connected");
+      })
+      .catch((err) => console.log("Error:: " + err));
+  }
+
+  static getInstance() {
+    if (!Database.instance) {
+      Database.instance = new Database();
     }
-    static getInstance() {
-        if (!Database.intance) {
-            Database.instance = new Database()
-        }
-        return Database.instance
-    }
+    return Database.instance;
+  }
 }
-const instanceMongodb = Database.getInstance()
-module.exports = instanceMongodb
+
+const instanceMongodb = Database.getInstance();
+module.exports = instanceMongodb;
